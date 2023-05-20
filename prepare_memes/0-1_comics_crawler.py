3ff749d98f23636f55717ef11a46223337650402
sngjuk <sngjuk@gmail.com>
2023-05-20 19:40:45 +0900

    fix bugs

--- prepare_memes/0-1_comics_crawler.py
+++ prepare_memes/0-1_comics_crawler.py
@@ -9,30 +9,30 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-def crawl_naver_webtoon(episode_url):
+
+base_dir = "downloaded"
+
+def crawl_naver_webtoon(episode_url, no):
     html = requests.get(episode_url).text
     soup = BeautifulSoup(html, 'html.parser')
 
-    comic_title = ' '.join(soup.select('.comicinfo h2')[0].text.split())
-    ep_title = ' '.join(soup.select('.tit_area h3')[0].text.split())
-
     for img_tag in soup.select('.wt_viewer img'):
-        image_file_url = img_tag['src']
-        image_dir_path = os.path.join(os.path.dirname(__file__), comic_title, ep_title)
-        image_file_path = os.path.join(image_dir_path, os.path.basename(image_file_url))
+        url = img_tag['src']
+        base_path = os.path.join(os.path.dirname(__file__), base_dir, f'{no:03d}')
+        file_path = os.path.join(base_path, os.path.basename(url))
 
-        if not os.path.exists(image_dir_path):
-            os.makedirs(image_dir_path)
+        if not os.path.exists(base_path):
+            os.makedirs(base_path)
 
-        print(image_file_path)
-
+        print(file_path)
         headers = {'Referer': episode_url}
-        image_file_data = requests.get(image_file_url, headers=headers).content
-        open(image_file_path, 'wb').write(image_file_data)
+        image_file_data = requests.get(url, headers=headers).content
+        open(file_path, 'wb').write(image_file_data)
 
-    print('Completed !')
+    print(f"{no} completed")
+
 
 if __name__ == '__main__':
-  for no in range(1,160):
-    episode_url = 'https://comic.naver.com/webtoon/detail.nhn?titleId=103759&no=' +str(no) +'&weekday=wed'
-    crawl_naver_webtoon(episode_url)
+  for no in range(1, 160):
+    episode_url = 'https://comic.naver.com/webtoon/detail?titleId=103759&no=' + str(no)
+    crawl_naver_webtoon(episode_url, no)
--- prepare_memes/0-2_cut_comics_into_memes.py
+++ prepare_memes/0-2_cut_comics_into_memes.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python3
 '''
 Comics cutting. (rect boxed toon is recommended)
-Usage : ./cutter.py --kumiko_dir=./kumiko/ --meme_dir=./input_original/ --out_dir=./output_cut/
+Usage : ./cutter.py --kumiko_path=./kumiko/ --input=./input_original/ --output=./output_cut/
 '''
+import argparse
+import json
 import os
+import pathlib
+import subprocess
 import sys
 from subprocess import call
-import subprocess
-import json
-import argparse
-import re
+
 import PIL.Image
-import pathlib
+
 
 def get_args_parser():
   parser = argparse.ArgumentParser(description='Directories for processing')
-  parser.add_argument('-k', '--kumiko_dir', type=str, default='./kumiko/', help='Directory of kumiko')
-  parser.add_argument('-i', '--meme_dir', type=str, required=True,help='Directory of a input toon(to be cut).')
-  parser.add_argument('-o', '--out_dir', type=str, required=True, help='Directory of a output cut.')
-
+  parser.add_argument('-k', '--kumiko_path', type=str, default='./kumiko/', help='kumiko path')
+  parser.add_argument('-i', '--input', type=str, default='./downloaded')
+  parser.add_argument('-o', '--output', type=str, default='./cut')
   args = parser.parse_args()  
   return args
+
 
 def resize_img(im, episode, in_dir):
   for h in range(0, im.height, 2200):
@@ -29,9 +30,10 @@ def resize_img(im, episode, in_dir):
     nim.save(in_dir+episode+'/'+"PartialImage-" + num + ".jpg")
     #display(Image('PartialImage-' +str(h) + '.jpg', width=300, height=300))
 
-#resize file for kumiko. maximum 2500 pix
+
+# resize img for kumiko. max size is 2500px
 def pre_cutter(args):
-  in_dir = os.path.abspath(args.meme_dir)+ '/'
+  in_dir = os.path.abspath(args.input)+ '/'
 
   episodes = os.listdir(in_dir)
   for episode in episodes:
@@ -52,43 +54,49 @@ def pre_cutter(args):
           resize_img(im, episode, in_dir)
           os.remove(in_dir+episode+'/'+img)
 
-def run_kumiko(args):
-  in_dir = os.path.abspath(args.meme_dir) + '/'
-  km_dir = os.path.abspath(args.kumiko_dir)+ '/'
 
+def run_kumiko(args):
+  in_dir = os.path.abspath(args.input) + '/'
+  km_dir = os.path.abspath(args.kumiko_path) + '/'
+
+  plist = []
   episodes = os.listdir(in_dir)
   for episode in episodes:
     print(episode)
     imgs = os.listdir(in_dir+episode)
-    delist = [x for x in imgs if not x.lower().endswith(('.jpg','.jpeg','.png'))]
-    print('delete non image files(.jpg, .jpeg, .png) for kumiko module. : ' +str(delist))
+    delist = [x for x in imgs if not x.lower().endswith(('.jpg', '.jpeg', '.png'))]
+    print('deleted non image files(.jpg, .jpeg, .png) : ' + str(delist))
     for i in delist:
       call(["rm", "-rf", str(in_dir+str(episode)+'/'+i)])
 
-    proc = subprocess.Popen([str(km_dir)+'kumiko', '-i', str(in_dir)
-                             +''+str(episode), '-o', str(in_dir)+str(episode)
-                             +'/kumiko_res.json'], stdout=subprocess.PIPE) 
-    output = proc.stdout.read()
-    
+    p = subprocess.Popen([str(km_dir)+'kumiko', '-i', str(in_dir) + str(episode),
+                         '-o', str(in_dir)+str(episode) + '/kumiko_res.json'], stdout=subprocess.PIPE)
+    plist.append(p)
+
+  print('wait for subprocess to finish...')
+  exit_code = [p.wait() for p in plist]
+  print(exit_code)
+
+
 def run_cutter(args):  
-  in_dir = os.path.abspath(args.meme_dir)+ '/'
-  out_dir = os.path.abspath(args.out_dir)+ '/'
+  in_dir = os.path.abspath(args.input) + '/'
+  out_dir = os.path.abspath(args.output) + '/'
 
   episodes = os.listdir(in_dir)
-  ep_num = 0
   if not os.path.exists(out_dir):
     os.makedirs(out_dir)
-  
+
   for idx, episode in enumerate(episodes):
-    f = open(str(in_dir)+str(episode)+'/kumiko_res.json')
+    episode_path = str(in_dir)+str(episode)
+    f = open(episode_path + '/kumiko_res.json')
     json_string = f.read()
     f.close()
-    
+
     objs = json.loads(json_string)
     inum=0
 
     epi_name = episode.replace(' ', '_').replace('-','_')
-    print('Current episode : ' +str(epi_name))
+    print('Current episode : ' + str(epi_name))
 
     directory = out_dir + epi_name
     if not os.path.exists(directory):
@@ -102,15 +110,16 @@ def run_cutter(args):
         y = i[1]
         w = i[2]
         h = i[3]
-        call(["convert", obj['filename'],"-quality", "100%", "-crop",
-              str(w)+'x'+str(h)+'+'+str(x)+'+'+str(y),
-              out_dir + epi_name +'/'+epi_name +'-'+ '%03d'%inum + sfx ])
+        p = call(["convert", obj['filename'], "-quality", "100%", "-crop",
+                 str(w)+'x'+str(h)+'+'+str(x)+'+'+str(y),
+                 out_dir + epi_name + '/' +epi_name +'-'+ '%03d'%inum + sfx ])
         inum+=1
+
 
 def main(args):
   print("""\n[Warning] This script may Slice images into pieces and Delete non-image files in directory. 
   (supported type: .jpg .jpeg .png) \n Make sure you're using copy of original folder.""")
-  print('Are you sure to continue ? (y/n) : ' ,end='')
+  print('Are you sure to continue ? (y/n) : ', end='')
   ans = input()
   if not ans == 'y':
     print('Aborted !')
@@ -119,10 +128,11 @@ def main(args):
   print('\n## Phase 1: Image preprocessing...(resizing)\n')
   pre_cutter(args)
   print('\n## Phase 2: Detect scenes boxes...\n')
-  run_kumiko(args) # TODO : auto split cutting with specific size. = done.
+  run_kumiko(args)
   print('\n## Cut box slicing with kumiko_res.json...')
   run_cutter(args)
   print('Cutting done.\n')
+
 
 if __name__ == "__main__":
   args = get_args_parser()
